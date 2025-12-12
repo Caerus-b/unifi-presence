b25df550478ced26a92af51a3c3277ed6b20ab9c
Otis Smylie <otis@otis.ong>
2025-12-12 22:35:15 +0000

    added automatic site detection

--- app.py
+++ app.py
@@ -3,46 +3,53 @@ import requests
 import time
 import os
 
-# Read from environment variables from docker
-UNIFI_URL = os.getenv("UNIFI_URL")
-USERNAME = os.getenv("UNIFI_USER")
-PASSWORD = os.getenv("UNIFI_PASS")
-API_KEY = os.getenv("UNIFI_KEY")
-DEVICE_MAC = os.getenv("DEVICE_MAC")
+#UNIFI_URL = os.getenv("UNIFI_URL") # e.g https://10.10.10.10:8080
+#API_KEY = os.getenv("UNIFI_KEY") 
+#SITE_ID = os.getenv("UNIFI_SITE")
+#DEVICE_MAC = os.getenv("DEVICE_MAC")
+
+
 
 app = Flask(__name__)
 session = requests.Session()
 session.verify = False  # ignore SSL warnings for self-signed certs
 
-def login():
-    payload = {"username": USERNAME, "password": PASSWORD}
-    r = session.post(f"{UNIFI_URL}/api/login", json=payload)
-    r.raise_for_status()
-    print("Logged in to UniFi controller.")
+def get_site_id():
+    global SITE_ID
+    if SITE_ID is None:
+        print("No site ID specified, detecting automatically")
+        res = session.get(f"{UNIFI_URL}/proxy/network/integration/v1/sites")
+        if res.status_code == 401:  
+            print("Failed to authenticate with Unifi Application.")
+        sites = res.json().get("data", [])
+        for site in sites:
+            print(f'Found Site: {site.get("name", "")} with ID: {site.get("id", "")}')
+        if sites:
+            SITE_ID = sites[0].get("id")
 
-def is_phone_connected():
+def is_device_live():
     try:
-        r = session.get(f"{UNIFI_URL}/proxy/network/api/s/default/stat/sta")
-        if r.status_code == 401:  # session expired
-            login()
-            r = session.get(f"{UNIFI_URL}/proxy/network/api/s/default/stat/sta")
-
-        clients = r.json().get("data", [])
+        res = session.get(f"{UNIFI_URL}/proxy/network/integration/v1/sites/{SITE_ID}/clients")
+        if res.status_code == 401:  
+            print("Failed to authenticate with Unifi Application.")
+        clients = res.json().get("data", [])
         for client in clients:
-            if client.get("mac", "").lower() == PHONE_MAC.lower():
+            print(client.get("name"))
+            if client.get("mac", "").lower() == DEVICE_MAC.lower():  # Filter response data entries for device mac address
                 return True
         return False
-    except Exception as e:
-        print("Error checking UniFi:", e)
+    except Exception as error:
+        print("Error checking UniFi:", error)
         return False
 
-@app.route("/presence")
-def presence_api():
+@app.route("/")
+def route():
     return jsonify({
-        "phone_nearby": is_phone_connected(),
+        "phone_nearby": is_device_live(),
         "timestamp": int(time.time())
     })
 
 if __name__ == "__main__":
-    login()
+    if SITE_ID is None:
+        get_site_id()
     app.run(host="0.0.0.0", port=5000)
